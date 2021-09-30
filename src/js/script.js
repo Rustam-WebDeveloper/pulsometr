ded2d9245a7abad171ab68f6cbe67f3bb1ad7bfe
rustam-webdeveloper <wellfixed@mail.ru>
2021-09-30 20:33:02 +0300

    добавил прокрутку

--- src/js/script.js
+++ src/js/script.js
@@ -85,7 +85,7 @@ $(document).ready(function(){
         validateForm('#consultation form');
         validateForm('#order form');
 
-        $('input [name=phone]').mask("+7 (999) 999-99-99");
+        $('input [name=phone]').mask("+7(999)999-99-99");
 
         $('form').submit(function(e){
             e.preventDefault();
@@ -102,4 +102,20 @@ $(document).ready(function(){
             });
             return false;
         });
+
+        //scrol
+
+        $(window).scroll(function(){
+            if($(this).scrollTop()>1600){
+                $('.pageup').fadeIn();
+            }else{
+                $('.pageup').fadeOut();
+            }
+        });
+
+        $("a[href^='#'").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+        })
   });
